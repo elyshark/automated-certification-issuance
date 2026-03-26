dc95ce953d05e21e6e77c5dadcda38ed961f00fc
Elysha Soffiea Shariful Basri <234398869+elyshark@users.noreply.github.com>
2026-03-26 16:31:33 +0800

    fix scroll on drawers

--- src/lib/components/ui/drawer/drawer-content.svelte
+++ src/lib/components/ui/drawer/drawer-content.svelte
@@ -2,6 +2,7 @@
   import { Drawer as DrawerPrimitive } from "vaul-svelte";
   import DrawerPortal from "./drawer-portal.svelte";
   import DrawerOverlay from "./drawer-overlay.svelte";
+  import { ScrollArea } from "$lib/components/ui/scroll-area";
   import { cn } from "$lib/utils";
   import type { ComponentProps } from "svelte";
   import type { WithoutChildrenOrChild } from "$lib/utils";
@@ -26,8 +27,8 @@
       "group/drawer-content fixed z-50 flex h-auto flex-col bg-background",
       "data-[vaul-drawer-direction=top]:inset-x-0 data-[vaul-drawer-direction=top]:top-0 data-[vaul-drawer-direction=top]:mb-24 data-[vaul-drawer-direction=top]:max-h-[80vh] data-[vaul-drawer-direction=top]:rounded-b-lg data-[vaul-drawer-direction=top]:border-b",
       "data-[vaul-drawer-direction=bottom]:inset-x-0 data-[vaul-drawer-direction=bottom]:bottom-0 data-[vaul-drawer-direction=bottom]:mt-24 data-[vaul-drawer-direction=bottom]:max-h-[80vh] data-[vaul-drawer-direction=bottom]:rounded-t-lg data-[vaul-drawer-direction=bottom]:border-t",
-      "data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:end-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-s data-[vaul-drawer-direction=right]:sm:max-w-sm",
-      "data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:start-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-e data-[vaul-drawer-direction=left]:sm:max-w-sm",
+      "data-[vaul-drawer-direction=right]:inset-y-0 data-[vaul-drawer-direction=right]:inset-e-0 data-[vaul-drawer-direction=right]:w-3/4 data-[vaul-drawer-direction=right]:border-s data-[vaul-drawer-direction=right]:sm:max-w-sm",
+      "data-[vaul-drawer-direction=left]:inset-y-0 data-[vaul-drawer-direction=left]:inset-s-0 data-[vaul-drawer-direction=left]:w-3/4 data-[vaul-drawer-direction=left]:border-e data-[vaul-drawer-direction=left]:sm:max-w-sm",
       className,
     )}
     {...restProps}
@@ -35,6 +36,16 @@
     <div
       class="mx-auto mt-4 hidden h-2 w-[100px] shrink-0 rounded-full bg-muted group-data-[vaul-drawer-direction=bottom]/drawer-content:block"
     ></div>
-    {@render children?.()}
+    <ScrollArea class="min-h-0 flex-1">
+      <div class="flex min-h-full flex-col">
+        {@render children?.()}
+      </div>
+    </ScrollArea>
   </DrawerPrimitive.Content>
 </DrawerPortal>
+
+<style>
+  :global([data-slot="drawer-footer"]) {
+    margin-top: auto;
+  }
+</style>
